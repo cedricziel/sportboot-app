059b84fd71c501c0b7b6c3e2caa5065c593d8373
Cedric Ziel <cedric@cedric-ziel.com>
2025-10-05 22:49:24 +0200

    fix: delegates

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -2,15 +2,7 @@ import Cocoa
 import FlutterMacOS
 
 @main
-class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
-  override func applicationDidFinishLaunching(_ notification: Notification) {
-    super.applicationDidFinishLaunching(notification)
-    // Set notification delegate for macOS 10.14+
-    if #available(macOS 10.14, *) {
-      UNUserNotificationCenter.current().delegate = self
-    }
-  }
-
+class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
